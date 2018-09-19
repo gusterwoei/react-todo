7f372bc8ed885da9f9f3f78b775c34daa18c6902
Gricher <seongwoei.chua@veltra.com>
2018-09-19 17:03:22 +0800

    first commit

---
+++ src/components/app/app.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import './app.css';
+import logo from '../../logo.svg';
+
+export default class App extends React.Component {
+	
+	public render() {
+      return (
+         <div className="App">
+            <header className="header">
+               <img src={logo} className="logo" alt="logo" />
+               <h1 className="title">Welcome to React</h1>
+            </header>
+            <p className="intro">
+               To get started, edit <code>src/App.tsx</code> and save to reload.
+            </p>
+         </div>
+      );
+   }
+};
---
+++ src/components/todo-list-page/todo-list-page.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Component } from 'react'
+import './todo-list-page.scss'
+
+export default class TodoListPage extends Component {
+	
+	public render() {
+		return (
+			<div className='todo-list-page'>
+				<div className='toolbar'> </div>
+			</div>
+		)
+	}
+}
